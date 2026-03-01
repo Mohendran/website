20e123e78f93ae8374630bfecdce9ea08005123b
Mohendran <mohendran@Kowsalyas-MacBook-Pro.local>
2026-03-01 12:09:10 +0530

    Github Pages Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mohendran.github.io',
-  base: '/website',
+  base: '/',
 	integrations: [tailwind()],
 	// output: 'server',
   // adapter: vercel({
